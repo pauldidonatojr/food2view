d0d0dcad398767ebe9ff98d1270cf025ffecac5b
pauldidonatojr <pjdidonatojr@gmail.com>
2022-04-30 18:16:37 -0400

    blue commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,16 +13,58 @@ const Hero = () => {
 
 const Wrapper = styled.section`
  padding: 1rem;
- min-height: 80vh;
+ height: 100%;
+ min-height: 30rem;
  display: grid;
  align-items: center;
  justify-content: center;
 
- main {
-  margin-top: 2rem;
-  height: 45vh;
-  width: 40vh;
-  background-color: blue;
+ /* In Size Order */
+
+ /* Extra small devices (phones, 600px and down) */
+ @media only screen and (max-width: 600px) {
+  main {
+   margin-top: 0.5rem;
+   height: 45vh;
+   width: 40vh;
+   background-color: blue;
+  }
+ }
+ /* Small devices (portrait tablets and large phones, 600px and up) */
+ @media only screen and (min-width: 600px) {
+  main {
+   margin-top: 0.5rem;
+   height: 45vh;
+   width: 40vh;
+   background-color: green;
+  }
+ }
+/* Medium devices *landscape tablets, 768px and up) */
+ @media only screen and (min-width: 768px) {
+  main {
+   margin-top: 0.5rem;
+   height: 45vh;
+   width: 40vh;
+   background-color: orange;
+  }
+ }
+/* Large devices (lapto/desktops, 992px and up) */
+ @media only screen and (min-width: 992px) {
+  main {
+   margin-top: 0.5rem;
+   height: 45vh;
+   width: 40vh;
+   background-color: purple;
+  }
+ }
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+ @media only screen and (min-width: 1200px) {
+  main {
+   margin-top: 0.5rem;
+   height: 45vh;
+   width: 40vh;
+   background-color: red;
+  }
  }
 `
 
